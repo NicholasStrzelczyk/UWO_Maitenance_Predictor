53fc47452db5929208b8ca6516aaf03106aefa44
Nicholas Strzelczyk <17nas6@queensu.ca>
2024-06-29 00:53:44 -0400

    added data list generator

---
+++ data_list_generator.py
@@ -0,0 +1,24 @@
+import os
+
+from tqdm import tqdm
+
+if __name__ == '__main__':
+	# hyperparameters
+	data_dir = "/Users/nick_1/Bell_5G_Data/1080_snapshots/train"
+	list_name = "list.txt"
+	start_date = 13
+	end_date = 28
+
+	# ----- begin generating list ----- #
+	list_file = open(os.path.join(data_dir, list_name), "a")
+
+	for day in tqdm(range(start_date, end_date + 1), desc='Generating data list'):
+		date_string = "2024_06_{}".format(day)
+
+		for f_name in os.listdir(os.path.join(data_dir, "images")):
+			if date_string in f_name:
+				img_path = os.path.join(data_dir, "images", f_name)
+				tgt_path = os.path.join(data_dir, "targets", "LABEL_{}.png".format(date_string))
+				list_file.write(img_path + " " + tgt_path + "\n")
+
+	list_file.close()
