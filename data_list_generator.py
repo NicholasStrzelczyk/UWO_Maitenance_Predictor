e0f0010a08537298c30d3399c37406974c51e800
Nicholas Strzelczyk <17nas6@queensu.ca>
2024-07-01 16:19:34 -0400

    made small fixes to data_list_generator

--- data_list_generator.py
+++ data_list_generator.py
@@ -5,11 +5,17 @@ from tqdm import tqdm
 
 if __name__ == '__main__':
     # hyperparameters
-    # data_dir = "/Users/nick_1/Bell_5G_Data/1080_snapshots/train"  # mac
-    data_dir = "C:\\Users\\NickS\\UWO_Summer_Research\\Bell_5G_Data\\1080_snapshots\\train"  # windows
-    list_name = "list_{}.txt".format(sys.platform)
-    start_date = 13
-    end_date = 28
+    start_date = 29
+    end_date = 30
+    partition = 'test'
+
+    # get data paths depending on platform
+    if sys.platform == 'darwin':  # mac
+        data_dir = "/Users/nick_1/Bell_5G_Data/1080_snapshots/{}".format(partition)
+        list_name = "list_{}.txt".format(sys.platform)
+    else:  # windows
+        data_dir = "C:\\Users\\NickS\\UWO_Summer_Research\\Bell_5G_Data\\1080_snapshots\\{}".format(partition)
+        list_name = "list_{}.txt".format(sys.platform)
 
     # ----- begin generating list ----- #
     list_file = open(os.path.join(data_dir, list_name), "a")
