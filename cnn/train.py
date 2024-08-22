2e2a93ec0f0344fc7c102259289700277a98d55f
Nicholas Strzelczyk <17nas6@queensu.ca>
2024-08-22 16:29:10 -0400

    retrying model 3

--- cnn/train.py
+++ cnn/train.py
@@ -144,7 +144,8 @@ if __name__ == '__main__':
     # optimizer = torch.optim.SGD(params=model.parameters(), lr=1e-3)  # got zeros for metrics
     # optimizer = torch.optim.SGD(params=model.parameters(), lr=1e-5)  # changes are very slow but not zero...
     # optimizer = torch.optim.SGD(params=model.parameters(), lr=1e-4)  # still got zeros...
-    optimizer = torch.optim.SGD(params=model.parameters(), lr=1e-6)  # current
+    optimizer = torch.optim.SGD(params=model.parameters(), lr=1e-6)  # sooooo slow...
+    optimizer = torch.optim.SGD(params=model.parameters(), lr=1e-6, momentum=0.9)  # current
 
     # run torch summary report
     summary(model, input_size=(3, input_shape[0], input_shape[1]))
