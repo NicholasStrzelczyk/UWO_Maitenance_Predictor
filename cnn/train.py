3e8f241ff36944e5d1e66ae8bded39f48ae1b81c
Nicholas Strzelczyk <17nas6@queensu.ca>
2024-08-22 20:35:40 -0400

    retrying model 3

--- cnn/train.py
+++ cnn/train.py
@@ -147,7 +147,7 @@ if __name__ == '__main__':
     # optimizer = torch.optim.SGD(params=model.parameters(), lr=1e-6)  # sooooo slow...
     # optimizer = torch.optim.SGD(params=model.parameters(), lr=1e-6, momentum=0.9)  # still too slow
     # optimizer = torch.optim.SGD(params=model.parameters(), lr=1e-6, momentum=0.99)  # converges to zero f1
-    # optimizer = torch.optim.SGD(params=model.parameters(), lr=1e-7, momentum=0.99)  # best trial yet
+    # optimizer = torch.optim.SGD(params=model.parameters(), lr=1e-7, momentum=0.99)  # best trial yet (model_4)
 
     # run torch summary report
     summary(model, input_size=(3, input_shape[0], input_shape[1]))
